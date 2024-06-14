18767497d1c7ed4a2bfcc94bcef2b04b602f4ab3
wperitz <wgp3aq@virginia.edu>
2024-06-14 14:02:12 -0400

    Added problem 2

--- project-euler.py
+++ project-euler.py
@@ -1,11 +1,37 @@
+import numpy
+import pandas
+import matplotlib
+
+################################################
 # Problem 1
 
-sum = 0
+sum1 = 0
 n = 1
 
 while n < 1000:
     if n % 3 == 0 or n % 5 == 0:
-        sum += n
+        sum1 += n
     n += 1
 
-print(sum)
+print(sum1)
+
+
+################################################
+# Problem 2
+
+x = 1
+y = 2
+sum2 = 0
+
+while y <= 4000000:
+    if y % 2 == 0:
+        sum2 += y
+    x, y = y, x + y 
+
+print(sum2)
+
+
+################################################
+# Problem 3
+
+
